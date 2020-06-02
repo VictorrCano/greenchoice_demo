24eab2e135bd2ce81a7797dbc27c5b5a3e3b7802
VictorrCano <46689150+VictorrCano@users.noreply.github.com>
2020-06-02 17:10:37 -0500

    fixed state in App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,38 @@ import React from 'react';
 import Todo from './components/Todo.js';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+ 
+  state={
+    todo:[
+      {
+      id: 1,
+      title:'Do laundry',
+      completed: false
+      },
+      {
+      id: 2,
+      title:'Wash dishes',
+      completed: false
+      },
+      {
+      id: 3,
+      title:'Walk dog',
+      completed: false
+      },
+    ]
+  }
 
-  
-  return (
+  render() {
+
     
-    <div className="App">
-      <Todo/>
-    </div>
-  );
+    return (
+      
+      <div className="App">
+        <Todo todo={this.state.todo}/>
+      </div>
+    );
+  }
 }
 
 export default App;
