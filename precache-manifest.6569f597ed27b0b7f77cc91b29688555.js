dff990cd2b5ec8f5bdfa5c55c551d8475041aab6
VictorrCano <46689150+VictorrCano@users.noreply.github.com>
2020-06-04 18:02:25 -0500

    Updates

---
+++ precache-manifest.6569f597ed27b0b7f77cc91b29688555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51f74b7d9212017ef5c5913740af046",
+    "url": "/greenchoice_demo/index.html"
+  },
+  {
+    "revision": "5844e9785d80e0c5da98",
+    "url": "/greenchoice_demo/static/css/main.d325696e.chunk.css"
+  },
+  {
+    "revision": "ec0040d479ce7570f72a",
+    "url": "/greenchoice_demo/static/js/2.96b19f64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greenchoice_demo/static/js/2.96b19f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5844e9785d80e0c5da98",
+    "url": "/greenchoice_demo/static/js/main.ced0139a.chunk.js"
+  },
+  {
+    "revision": "f8f87012182b68ba0aa4",
+    "url": "/greenchoice_demo/static/js/runtime-main.bfa3fbff.js"
+  }
+]);
